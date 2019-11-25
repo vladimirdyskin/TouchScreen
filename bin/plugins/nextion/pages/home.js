315ddf75f404603865f28c129ce5dcb66bd2e8cc
vladimirdyskin <vladimirdyskin@me.com>
2019-11-25 18:37:14 +0300

    123456

--- bin/plugins/nextion/pages/home.js
+++ bin/plugins/nextion/pages/home.js
@@ -82,7 +82,7 @@ var Home = function (_abstract) {
                   var result = (0, _requestPromiseNative2.default)({
                   uri: global.SERVER_URL + "/gcode",
                   formData: {
-                    'gcode': "$H\n[[Delay 3]]\n G1X-13.7F300\n"
+                    'gcode': "$H\n"
                   },
                     method: 'POST'
                   });
